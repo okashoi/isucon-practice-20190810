41ba100cffad84a7fb4284d3c9fc9d57347f3504
MasamiMishima <mishima.masami@willgate.co.jp>
2019-08-10 17:09:16 +0900

    fix:

--- go/app.go
+++ go/app.go
@@ -355,16 +355,16 @@ LIMIT 10`, user.ID)
 		}
 	}
 	rows.Close()
-	rows, err = db.Query(`SELECT * FROM comments ORDER BY created_at DESC LIMIT 1000`)
+	rows, err = db.Query(`SELECT * FROM comments WHERE comments.user_id IN (?) ORDER BY created_at DESC LIMIT 1000`, strings.Join(friendIds, ","), user.ID)
 //	rows, err = db.Query(`SELECT
 //comments.id, comments.entry_id, comments.user_id,comments.comment, comments.created_at
 //FROM comments
 //JOIN entries ON comments.entry_id = entries.id
 //WHERE comments.user_id IN (?)
 //AND (
-//  entries.private = 0
-//  OR (entries.private = 1
-//  AND (entries.user_id = ? OR entries.user_id IN (?)))
+// entries.private = 0
+// OR (entries.private = 1
+// AND (entries.user_id = ? OR entries.user_id IN (?)))
 //) ORDER BY comments.id DESC LIMIT 10`,strings.Join(friendIds, ","), user.ID, strings.Join(friendIds, ","))
 	if err != sql.ErrNoRows {
 		checkErr(err)
@@ -373,9 +373,7 @@ LIMIT 10`, user.ID)
 	for rows.Next() {
 		c := Comment{}
 		checkErr(rows.Scan(&c.ID, &c.EntryID, &c.UserID, &c.Comment, &c.CreatedAt))
-		if !isFriend(w, r, c.UserID) {
-			continue
-		}
+		
 		row := db.QueryRow(`SELECT * FROM entries WHERE id = ?`, c.EntryID)
 		var id, userID, private int
 		var body string
